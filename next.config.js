38a3390b98be7c3c618b42add5ce14869bf08b9b
dohye1 <tsc03018@gmail.com>
2024-03-31 14:58:08 +0900

    :sparkles: Pwa 설정

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+const settings = {
+  env: {},
+  devIndicators: {
+    autoPrerender: false,
+  },
+  pwa: {
+    dest: "public",
+  },
+};
+
+module.exports =
+  process.env.NODE_ENV === "development" ? settings : withPWA(settings);
